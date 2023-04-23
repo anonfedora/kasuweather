319b5a6269f49f866254375a9473bdf8d933fd15
Musa Eleazar <musaeleazar090@gmail.com>
2023-04-23 14:23:48 +0100

    Software Engineering First Project (First commit) - Group-9

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3aee3f758amsh7e7d167ce9ab510p156941jsnc806d6dbedfd",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e9c2ba22b914cff8fcfcb116e86558c7";
---
+++ src/components/logos/logos.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Logos = () => {
+  return (
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "-20px",
+        marginTop: "-50px",
+        marginBottom: "-10px",
+      }}
+    >
+      <img
+        alt="logo"
+        className="icon-kasu"
+        src={`icons/logos.png`}
+        style={{ height: "110px", width: "110px" }}
+      />
+      <img
+        alt="logo"
+        className="icon-group-9"
+        src={`icons/SE_logos.png`}
+        style={{
+          height: "100px",
+          width: "100px",
+          marginTop: "-15px",
+          marginBottom: "-20px",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Logos;
